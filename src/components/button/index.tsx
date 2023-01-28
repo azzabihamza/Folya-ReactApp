dcf487a144005512ce1ae7e8c8e71fd9f8b6d9c4
azzabihamza <hamza.azzabi@esprit.tn>
2023-01-28 20:25:48 +0000

    setup project

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { FC } from "react";
+import './styles.module.scss';
 
-export type IButton = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
+interface Props {
+    label: string;
+    onClick: () => void;
+    disabled?: boolean;
+    variant?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link';
+}
 
-export const Button: React.FC<IButton> = ({
-  className = "",
-  children,
-  ...rest
-}) => {
-  return (
-    <button
-      className={`py-2 px-4 rounded bg-green-500 hover:bg-green-600 focus:outline-none ring-opacity-75 ring-green-400 focus:ring text-white text-lg ${className}`}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
+const Button: FC<Props> = ({ label, onClick , disabled=false , variant = 'primary' }) => {
+    return (
+        <button className={`btn btn-${variant} py-2 px-4 rounded bg-green-500 hover:bg-green-600 focus:outline-none ring-opacity-75 ring-green-400 focus:ring text-white text-lg`} onClick={onClick} disabled={disabled}>
+            {label}
+        </button>
+    );
 };
+
+export default Button;
