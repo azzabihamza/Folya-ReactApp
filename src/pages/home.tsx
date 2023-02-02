1ec220963a7eddcc4529d8b420cea6f52fbfb920
azzabihamza <hamza.azzabi@esprit.tn>
2023-02-02 10:05:36 +0000

    add pages

---
+++ src/pages/home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Container, Modal, Button, Input, Card } from "components";
+import  useModal  from "hooks/useModal";
+
+
+const Home: React.FC = () => {
+  const { isOpen, toggle } = useModal();
+  return (
+    <Container>
+      <Button onClick={toggle} bgColor="red" >toggle</Button>
+      <Modal  title="test" isOpen={isOpen} toggle={toggle} />
+      <Input label="test" placeholder="test" value="test"  />
+      <Card className="block max-w-sm">test card </Card>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/pages/root.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+interface RootPageProps {
+    children: React.ReactNode;
+}
+
+export const RootPage: FC<RootPageProps> = ({ children }) => {
+    return (
+        <>
+        </>
+    )
+}
